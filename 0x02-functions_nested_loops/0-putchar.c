028c922d2930f9dc3a0f375f3bfffb6b949c47f4
established001 <blaccvibes@gmail.com>
2022-09-13 20:12:02 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print putchar
+ *
+ *
+ * Return: Always (0)
+ */
+
+int main(void) {
+
+char myArray[10] = "_putchar";
+int i;
+
+for(i = 0; i < 8; i++) {
+    printf("%s\n", myArray);
+    return(0);
+}
